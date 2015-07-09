d16e7497fd61f783a7c66f790a34af4ec7dad29c
Andrew Hemming <andrew.hemming@it.ox.ac.uk>
2015-07-09 15:27:24 +0100

    Changed vagrant fqdn

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,5 @@
 Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin" }
 
-node 'ewa-dev.oucs.ox.ac.uk', 'ewa-vm.oucs.ox.ac.uk' {
+node 'ewa-dev.oucs.ox.ac.uk', 'ewa.vm' {
     include ewa
 }
